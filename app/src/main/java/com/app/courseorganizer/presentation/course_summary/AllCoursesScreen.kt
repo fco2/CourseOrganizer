f23a7c4714e556916ead985b9a02fb786cc680cb
Chuka <obiayafc@gmail.com>
2022-06-08 08:12:55 -0400

    Added cards using surface to courses list items

--- app/src/main/java/com/app/courseorganizer/presentation/course_summary/AllCoursesScreen.kt
+++ app/src/main/java/com/app/courseorganizer/presentation/course_summary/AllCoursesScreen.kt
@@ -1,13 +1,18 @@
 package com.app.courseorganizer.presentation.course_summary
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -21,16 +26,24 @@ fun AllCoursesScreen(
 
         LazyColumn {
             items(viewModel.courses){ course ->
-                Row(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .padding(10.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceEvenly
+
+                Surface(
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = 10.dp,
+                    modifier = modifier.padding(5.dp),
                 ) {
-                    Text(text = course.title)
-                    Text(text = course.courseCode)
-                    Text(text = course.notes!!)
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Text(text = course.title, fontSize = 14.sp)
+                        Text(text = course.courseCode, fontSize = 14.sp)
+                        Text(text = course.notes!!, fontSize = 14.sp)
+                    }
                 }
             }
         }
